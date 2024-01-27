ff526d297c986c7594e5b355980a8041488ac648
andrii <andrii.klok@gmail.com>
2024-01-27 20:25:59 +0100

    Prototype

--- Patterns/Program.cs
+++ Patterns/Program.cs
@@ -5,6 +5,7 @@ using Patterns.Composite;
 using Patterns.Decorator;
 using Patterns.FactoryMethod;
 using Patterns.Mediator;
+using Patterns.Prototype;
 using System;
 using System.Collections.Generic;
 
@@ -18,16 +19,19 @@ namespace Patterns
 			var instance = Singleton.Instance;
 
 			//FactoryMethod
+			Console.WriteLine("<==Factory method==>");
 			List<Document> documents = new List<Document>();
 			documents.Add(new Resume());
 			documents.Add(new Report());
 			showFactory(documents);
 
 			//AbstractFactory
+			Console.WriteLine("\n<==Abstract factory==>");
 			new AbstractFactoryClient(new SedanFacory());
 			new AbstractFactoryClient(new SUVFactory());
 
 			//Bridge
+			Console.WriteLine("\n<==Bridge==>");
 			AbstractMessage longMessage = new LongMessage(new EmailMessageSender());
 			longMessage.SendMessage("long message!");
 
@@ -35,6 +39,7 @@ namespace Patterns
 			shortMessae.SendMessage("short sms!");
 
 			//Mediator
+			Console.WriteLine("\n<==Mediator==>");
 			var mediator = new ConcreteMediator();
 			var c1 = new Colleague1();
 			var c2 = new Colleague2();
@@ -49,6 +54,7 @@ namespace Patterns
 			c2.Send("bye bye");
 
 			//Decorator
+			Console.WriteLine("\n<==Decorator==>");
 			Margherita pizza = new Margherita();
 			Console.WriteLine("Margherita: " + pizza.GetPrice().ToString());
 
@@ -62,12 +68,35 @@ namespace Patterns
 			Console.WriteLine("Margherita with extra cheese + mushrooms + jalapeno: " + moreJalapeno.GetPrice().ToString());
 
 			//Composite
+			Console.WriteLine("\n<==Composite==>");
 			Pack pack = new Pack();
 			pack.Add(new Chips());
 			pack.Add(new Nachos());
 			pack.Add(new Juice());
 
-			Console.Write($"Total: {pack.GetPrice()}");
+			Console.Write($"Total: {pack.GetPrice()}\n");
+
+			//Protorype
+			Console.WriteLine($"\n<==Prototype==>");
+			Person p1 = new Person()
+			{
+				Name = "Person 1",
+				Age = 20,
+				AdditionalInfo = new AdditionalInfo()
+				{
+					FavoriteNumber = 1
+				}
+			};
+			Person p2 = p1.ShallCopy();
+			Person p3 = p1.DeepCopy();
+
+			showPrototype(new List<Person> { p1, p2, p3 }, "before changes");
+
+			p1.Age = 21;
+			p1.Name = "Person n";
+			p1.AdditionalInfo.FavoriteNumber = 2;
+
+			showPrototype(new List<Person> { p1, p2, p3 }, "after changes");
 
 			Console.ReadLine();
 		}
@@ -81,6 +110,15 @@ namespace Patterns
 				{
 					Console.WriteLine(" " + page.GetType().Name);
 				}
+			}
+		}
+
+		static void showPrototype(List<Person> people, string option)
+		{
+			Console.WriteLine($"{option}:");
+			foreach (var person in people)
+			{
+				Console.WriteLine($"{person.Name}: {person.Age} years old, favorite number {person.AdditionalInfo.FavoriteNumber}.");
 			}
 		}
 
---
+++ Patterns/Prototype/Person.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Patterns.Prototype
+{
+	class Person
+	{
+		public string Name { get; set; }
+		public int Age { get; set; }
+		public AdditionalInfo AdditionalInfo { get; set; }
+
+		public Person ShallCopy()
+		{
+			return (Person)this.MemberwiseClone();
+		}
+		public Person DeepCopy()
+		{
+			Person clone = (Person)this.MemberwiseClone();
+
+			clone.Name = String.Copy(this.Name);
+			clone.AdditionalInfo = new AdditionalInfo()
+			{
+				FavoriteNumber = this.AdditionalInfo.FavoriteNumber,
+			};
+
+			return clone;
+		}
+	}
+
+	class AdditionalInfo
+	{
+		public int FavoriteNumber { get; set; }
+	}
+}
